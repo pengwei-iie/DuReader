b0c058ca3a7aaca86626cac38abcdf3380f0583b
MR_Black <201424020211@chd.edu.cn>
2019-09-06 15:59:47 +0800

    add self attention of passage

--- tensorflow/rc_model.py
+++ tensorflow/rc_model.py
@@ -91,7 +91,8 @@ class RCModel(object):
         # self._fusion()
         self._encode()
         self._match()
-        self._fuse()
+        self._self_attention()
+        # self._fuse()
         # self._self_attention()  # add self_attention
         self._decode()
         self._compute_loss()
@@ -172,15 +173,53 @@ class RCModel(object):
             self.match_p_encodes = tf.nn.dropout(self.match_p_encodes, self.dropout_keep_prob)
             # self.match_q_encodes = tf.nn.dropout(self.match_q_encodes, self.dropout_keep_prob)
 
-    def _fuse(self):
-        """
-        Employs Bi-LSTM again to fuse the context information after match layer
-        """
-        with tf.variable_scope('fusion_p'):
-            self.fuse_p_encodes, _ = rnn('bi-lstm', self.match_p_encodes, self.p_length,
+    def _self_attention(self):
+        # 双线性softmax
+        with tf.variable_scope('bi_linear'):
+            # 经过双向lstm，最后一维变成300
+            batch_add5 = tf.shape(self.match_p_encodes)[0]
+
+            # use xavier initialization
+
+            W_bi = tf.get_variable("W_bi", [self.hidden_size * 2, self.hidden_size * 2],
+                                   initializer=tf.contrib.layers.xavier_initializer())
+            # W_bi = tf.get_variable("W_bi", [self.hidden_size * 2, self.hidden_size * 2],
+            #                        initializer=tf.random_normal_initializer(mean=0.0, stddev=0.1, seed=None, dtype=tf.float32))
+            tmp = tf.reshape(self.match_p_encodes, [-1, self.hidden_size*2])
+            tmp = tf.matmul(tmp, W_bi)
+            tmp = tf.reshape(tmp, [batch_add5, -1, self.hidden_size*2])
+            # 以上就是通过reshape的方式进行双线性变化
+            before_softmax = tf.tanh(tf.matmul(tmp, self.match_p_encodes, transpose_b=True))     # b, n, n
+            L = tfu.mask_softmax(before_softmax, self.p['mask'])
+            # L = tf.nn.softmax(tf.matmul(tmp, self.fuse_p_encodes, transpose_b=True))
+            self.binear_passage = tf.matmul(L, self.match_p_encodes)
+            # self.binear_passage = tfu.fusion(self.fuse_p_encodes, self.binear_passage, self.hidden_size, name="binear")
+
+            # 将最后一维变成self.hidden_size
+            # self.binear_passage = tfu.dense(self.binear_passage, 1, "to_hidden_size")
+            # 需要再经过一个双向LISTM
+        with tf.variable_scope('self_attention'):
+            self.fina_passage, _ = rnn('bi-lstm', self.binear_passage, self.p_length,
                                          self.hidden_size, layer_num=1)  # 经过双向RNN,变成前向+后向，150+150
-        if self.use_dropout:
-            self.fuse_p_encodes = tf.nn.dropout(self.fuse_p_encodes, self.dropout_keep_prob)
+
+        # 对问题操作,自对其，后续
+        # W_q = tf.get_variable("W_q", [self.hidden_size * 2, self.hidden_size * 2],
+        #                        initializer=tf.contrib.layers.xavier_initializer())
+        # tmp = tf.reshape(self.fuse_q_encodes, [-1, self.hidden_size * 2])
+        # tmp = tf.matmul(tmp, W_q)
+        # tmp = tf.reshape(tmp, [batch_add5, -1, self.hidden_size * 2])   # b, q-len, hidden
+        # alpha = tf.nn.softmax(tmp)      # b, n_q, 300
+        # self.self_ques = alpha*self.fuse_q_encodes
+
+    # def _fuse(self):
+    #     """
+    #     Employs Bi-LSTM again to fuse the context information after match layer
+    #     """
+    #     with tf.variable_scope('fusion_p'):
+    #         self.fuse_p_encodes, _ = rnn('bi-lstm', self.match_p_encodes, self.p_length,
+    #                                      self.hidden_size, layer_num=1)  # 经过双向RNN,变成前向+后向，150+150
+    #     if self.use_dropout:
+    #         self.fuse_p_encodes = tf.nn.dropout(self.fuse_p_encodes, self.dropout_keep_prob)
 
     def _decode(self):
         """
@@ -192,7 +231,7 @@ class RCModel(object):
         with tf.variable_scope('same_question_concat'):
             batch_size = tf.shape(self.start_label)[0]
             concat_passage_encodes = tf.reshape(
-                self.fuse_p_encodes,
+                self.fina_passage,
                 [batch_size, -1, 2 * self.hidden_size]
             )       # fina_passage: b*5, len_p, hidden --> b, 5*len_p, hidden
 
