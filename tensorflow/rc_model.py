73ef1a1398d8c74f5bc9248999c92947ae2058aa
MR_Black <201424020211@chd.edu.cn>
2019-10-13 10:37:08 +0800

    doc_loss *2

--- tensorflow/rc_model.py
+++ tensorflow/rc_model.py
@@ -69,7 +69,7 @@ class RCModel(object):
         self.layer = args.layer
         self.head = args.head
         self.batch_size = args.batch_size
-        self.is_train = tf.cast(True, tf.bool)
+        # self.is_train = tf.cast(True, tf.bool)
 
         # session info
         sess_config = tf.ConfigProto()
@@ -131,6 +131,7 @@ class RCModel(object):
         self.end_label = tf.placeholder(tf.int32, [None])
         self.answer_index = tf.placeholder(tf.int32, [None, 2])
         self.answer_loss = tf.placeholder(tf.int32, [None])
+        self.is_train = tf.placeholder(tf.bool)
         # self.dropout_keep_prob = tf.placeholder(tf.float32)
 
     def _embed(self):
@@ -249,34 +250,34 @@ class RCModel(object):
         passage_score = tf.squeeze(tfu.dense(tmp, 1, False, 'second_dense'), axis=2)
         self.passage_score = tf.nn.softmax(passage_score)
 
-    def _self_attention(self):
-        # 双线性softmax
-        with tf.variable_scope('bi_linear'):
-            # 经过双向lstm，最后一维变成300
-            batch_add5 = tf.shape(self.fuse_p_encodes)[0]
-
-            # use xavier initialization
-
-            W_bi = tf.get_variable("W_bi", [self.hidden_size * 2, self.hidden_size * 2],
-                                   initializer=tf.contrib.layers.xavier_initializer())
-            # W_bi = tf.get_variable("W_bi", [self.hidden_size * 2, self.hidden_size * 2],
-            #                        initializer=tf.random_normal_initializer(mean=0.0, stddev=0.1, seed=None, dtype=tf.float32))
-            tmp = tf.reshape(self.fuse_p_encodes, [-1, self.hidden_size*2])
-            tmp = tf.matmul(tmp, W_bi)
-            tmp = tf.reshape(tmp, [batch_add5, -1, self.hidden_size*2])
-            # 以上就是通过reshape的方式进行双线性变化
-            before_softmax = tf.tanh(tf.matmul(tmp, self.fuse_p_encodes, transpose_b=True))     # b, n, n
-            L = tfu.mask_softmax(before_softmax, self.p['mask'])
-            # L = tf.nn.softmax(tf.matmul(tmp, self.fuse_p_encodes, transpose_b=True))
-            self.binear_passage = tf.matmul(L, self.fuse_p_encodes)
-            # self.binear_passage = tfu.fusion(self.fuse_p_encodes, self.binear_passage, self.hidden_size, name="binear")
-
-            # 将最后一维变成self.hidden_size
-            # self.binear_passage = tfu.dense(self.binear_passage, 1, "to_hidden_size")
-            # 需要再经过一个双向LISTM
-        with tf.variable_scope('self_attention'):
-            self.fina_passage, _ = rnn('bi-lstm', self.binear_passage, self.p_length,
-                                         self.hidden_size, layer_num=1)  # 经过双向RNN,变成前向+后向，150+150
+    # def _self_attention(self):
+    #     # 双线性softmax
+    #     with tf.variable_scope('bi_linear'):
+    #         # 经过双向lstm，最后一维变成300
+    #         batch_add5 = tf.shape(self.fuse_p_encodes)[0]
+    #
+    #         # use xavier initialization
+    #
+    #         W_bi = tf.get_variable("W_bi", [self.hidden_size * 2, self.hidden_size * 2],
+    #                                initializer=tf.contrib.layers.xavier_initializer())
+    #         # W_bi = tf.get_variable("W_bi", [self.hidden_size * 2, self.hidden_size * 2],
+    #         #                        initializer=tf.random_normal_initializer(mean=0.0, stddev=0.1, seed=None, dtype=tf.float32))
+    #         tmp = tf.reshape(self.fuse_p_encodes, [-1, self.hidden_size*2])
+    #         tmp = tf.matmul(tmp, W_bi)
+    #         tmp = tf.reshape(tmp, [batch_add5, -1, self.hidden_size*2])
+    #         # 以上就是通过reshape的方式进行双线性变化
+    #         before_softmax = tf.tanh(tf.matmul(tmp, self.fuse_p_encodes, transpose_b=True))     # b, n, n
+    #         L = tfu.mask_softmax(before_softmax, self.p['mask'])
+    #         # L = tf.nn.softmax(tf.matmul(tmp, self.fuse_p_encodes, transpose_b=True))
+    #         self.binear_passage = tf.matmul(L, self.fuse_p_encodes)
+    #         # self.binear_passage = tfu.fusion(self.fuse_p_encodes, self.binear_passage, self.hidden_size, name="binear")
+    #
+    #         # 将最后一维变成self.hidden_size
+    #         # self.binear_passage = tfu.dense(self.binear_passage, 1, "to_hidden_size")
+    #         # 需要再经过一个双向LISTM
+    #     with tf.variable_scope('self_attention'):
+    #         self.fina_passage, _ = rnn('bi-lstm', self.binear_passage, self.p_length,
+    #                                      self.hidden_size, layer_num=1)  # 经过双向RNN,变成前向+后向，150+150
 
         # 对问题操作,自对其，后续
         # W_q = tf.get_variable("W_q", [self.hidden_size * 2, self.hidden_size * 2],
@@ -329,19 +330,20 @@ class RCModel(object):
             batch_size = tf.shape(self.start_label)[0]
 
             if self.is_train is not None:
-                one_passage = tf.gather_nd(tf.reshape(self.fina_passage,
+                one_passage = tf.gather_nd(tf.reshape(self.fuse_p_encodes,
                                                       [batch_size, -1, tf.shape(self.p_emb)[1], 2 * self.hidden_size]),
                                            self.answer_index)
 
                 print('self.is_train')
             else:
+                self.logger.info('not_train')
                 doc_index = tf.argmax(self.passage_score, -1)       # batch
                 # fixme: 对doc进行维度变换
                 # doc_index = tf.expand_dims(doc_index, 1)
                 index = tf.expand_dims(tf.range(tf.shape(doc_index)[0]), 1)
                 doc_index = tf.expand_dims(doc_index, 1)
                 doc_index = tf.concat([index, doc_index], axis=1)
-                one_passage = tf.gather_nd(tf.reshape(self.fina_passage,
+                one_passage = tf.gather_nd(tf.reshape(self.fuse_p_encodes,
                                                       [batch_size, -1,
                                                        tf.shape(self.p_emb)[1], 2 * self.hidden_size]),
                                            doc_index)
@@ -381,12 +383,12 @@ class RCModel(object):
         self.end_loss = sparse_nll_loss(probs=self.end_probs, labels=self.end_label)
         self.doc_loss = sparse_nll_loss(probs=self.passage_score, labels=self.answer_loss)
 
-        self.print_docloss = tf.reduce_mean(self.doc_loss)
+        self.print_docloss = 2 * tf.reduce_mean(self.doc_loss)
         self.print_ansloss = tf.reduce_mean(tf.add(self.start_loss, self.end_loss))
 
         self.all_params = tf.trainable_variables()
         # self.loss = tf.reduce_mean(tf.add(self.start_loss, self.end_loss))
-        self.loss = tf.reduce_mean(tf.add(tf.add(self.start_loss, self.end_loss), self.doc_loss))
+        self.loss = tf.reduce_mean(tf.add(tf.add(self.start_loss, self.end_loss), 2 * self.doc_loss))
         if self.weight_decay > 0:
             with tf.variable_scope('l2_loss'):
                 l2_loss = tf.add_n([tf.nn.l2_loss(v) for v in self.all_params])
